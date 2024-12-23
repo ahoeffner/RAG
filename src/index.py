ea0e91ac0db1679a9734f70e6fce2d6ef1b1fef4
ahoeffner <alex@hoeffner.net>
2024-12-23 23:26:16 +0100

    .

--- src/index.py
+++ src/index.py
@@ -6,35 +6,60 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 DOCUMENTS = "data"
 
+
 def main() :
-	print("Loading");
+	'''
+	print("Loading")
 	loader = PyPDFDirectoryLoader(DOCUMENTS)
-
 	docs = loader.load()
-	docs = split(docs);
-	print("Loaded");
 
-	store(docs)
+	docs = RAG.splitText(docs)
+	RAG.create("database",docs)
+
 	print("Stored");
+	'''
+
+	rag = RAG("database")
+	docs:list[Document] = rag.query("can i specify return type in python")
+
+	for doc in docs :
+		print(doc)
+		print()
+		print()
+		print()
+		print()
 
 
-def store(docs:list[Document]) :
-	vectors = Chroma.from_documents(docs,embeddingFunction(),persist_directory="vectors")
+
+class RAG :
+	vectors:Chroma = None
 
 
-def split(docs:list[Document]) :
-	text_splitter = RecursiveCharacterTextSplitter(
-	chunk_size=800,  # Desired chunk size
-	chunk_overlap=80,  # Overlapping characters between chunks
-	length_function=len,
-	is_separator_regex=False)
-	return(text_splitter.split_documents(docs))
+	def __init__(self,location:str) :
+		self.vectors =	Chroma(persist_directory=location, embedding_function=RAG.__embeddingFunction())
 
-def embeddingFunction() :
-	embeddings = BedrockEmbeddings(
-		credentials_profile_name="default", region_name="eu-central-1"
-	)
-	return(embeddings);
+
+	def query(self,text:str) :
+		return(self.vectors.similarity_search(text))
+
+
+	def create(location:str, docs:list[Document]) :
+		Chroma.from_documents(docs,RAG.__embeddingFunction(),persist_directory=location)
+
+
+	def splitText(docs:list[Document]) :
+		text_splitter = RecursiveCharacterTextSplitter(
+		chunk_size=800,  # Desired chunk size
+		chunk_overlap=80,  # Overlapping characters between chunks
+		length_function=len,
+		is_separator_regex=False)
+		return(text_splitter.split_documents(docs))
+
+	def __embeddingFunction() :
+		embeddings = BedrockEmbeddings(
+			credentials_profile_name="default", region_name="eu-central-1"
+		)
+		return(embeddings)
 
 
 main()
